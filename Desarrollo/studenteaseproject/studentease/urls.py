51478921e130f165fe5ea37f83af760465d68c5e
Santiago <santiagomancomaya@gmail.com>
2025-04-26 17:59:23 -0500

    ADD: App accounting y landing url

--- Desarrollo/studenteaseproject/studentease/urls.py
+++ Desarrollo/studenteaseproject/studentease/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from accounting import views as accounting_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', accounting_views.landing, name='landing'),  # Landing page URL
 ]
